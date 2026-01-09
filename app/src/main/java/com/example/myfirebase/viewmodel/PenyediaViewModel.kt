4e5699df7dd827089ec1e2063a4e27af7ec953bb
dutabrahmana <dutalinggau25@gmail.com>
2026-01-09 17:36:45 +0700

    Menambahkan ekstensi aplikasiDataSiswa untuk akses repository dan mendaftarkan HomeViewModel serta EntryViewModel ke dalam Factory.

--- app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
@@ -1,12 +1,13 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.myfirebase.repositori.AplikasiDataSiswa
-import androidx.lifecycle.ViewModelProvider
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
 
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
                 AplikasiDataSiswa
         )
@@ -14,10 +15,13 @@ fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
-            HomeViewModel(aplikasiDataSiswa().container.repositorySiswa)
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
         }
+
         initializer {
-            EntryViewModel(aplikasiDataSiswa().container.repositorySiswa)
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
         }
-    }
-}
